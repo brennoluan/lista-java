0f3607b07328b0b1faa4b225811f08e77ecc2a1c
Brenno Luan <brennoluan8530@gmail.com>
2021-03-04 11:50:04 -0300

    Questão2

---
+++ src/Lista2/Questao2.java
@@ -0,0 +1,30 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Questao2 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite o 1° lado do triângulo: ");
+		int lado1 = entrada.nextInt();
+		System.out.print("Digite o 2° lado do triângulo: ");
+		int lado2 = entrada.nextInt();
+		System.out.print("Digite o 3° lado do triângulo: ");
+		int lado3 = entrada.nextInt();
+		
+		/*Escaleno é o que tem 3 lados diferentes,
+		equilatero os 3 lados iguais e 
+		o isoceles tem 2 lados iguais e 1 diferente*/
+		
+		if(lado1 == lado2 && lado1 == lado3)
+			System.out.println("Equilatero");
+		else if(lado1 != lado2 && lado2 != lado3 && lado3 != lado1)
+			System.out.println("Escaleno");
+		else
+			System.out.println("Isoceles");
+
+	}
+
+}
